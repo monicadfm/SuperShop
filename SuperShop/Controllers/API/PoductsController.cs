5ec9b943fe27ad0b255ee09739bf3c46b002bc3e
terumineko <miamonicamoura@gmail.com>
2026-09-09 23:28:58 +0100

    API products controller

---
+++ SuperShop/Controllers/API/PoductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperShop.Data;
+
+namespace SuperShop.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PoductsController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public PoductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(_productRepository.GetAll());
+        }
+    }
+}
