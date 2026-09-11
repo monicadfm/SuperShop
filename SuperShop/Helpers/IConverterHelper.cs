413bff912079119ac3e057ff23c88869e11b9b7f
terumineko <miamonicamoura@gmail.com>
2026-09-11 23:32:28 +0100

    ImageHelper and ConverterHelper

---
+++ SuperShop/Helpers/IConverterHelper.cs
@@ -0,0 +1,12 @@
+﻿using SuperShop.Data.Entities;
+using SuperShop.Models;
+
+namespace SuperShop.Helpers
+{
+    public interface IConverterHelper
+    {
+        Product ToProduct(ProductViewModel mode, string path, bool isNew);
+
+        ProductViewModel ToProductViewModel(Product product);
+    }
+}
--- SuperShop/Startup.cs
+++ SuperShop/Startup.cs
@@ -49,6 +49,10 @@ namespace SuperShop
 
             services.AddScoped<IUserHelper, UserHelper>();
 
+            services.AddScoped<IImageHelper, ImageHelper>();
+
+            services.AddScoped<IConverterHelper, ConverterHelper>();
+
             services.AddScoped<IProductRepository, ProductRepository>();
 
             services.AddControllersWithViews();
