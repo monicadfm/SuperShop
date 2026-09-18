47c0b652079a6669cb2bfeee80e7b86f2f10867e
terumineko <miamonicamoura@gmail.com>
2026-09-18 20:48:49 +0100

    Change user profile and password

---
+++ SuperShop/Models/ChangePasswordViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuperShop.Models
+{
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [Display(Name ="Current Password")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [Display(Name = "New Password")]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [Compare("New Password")]
+        public string Confirm { get; set; }
+    }
+}
