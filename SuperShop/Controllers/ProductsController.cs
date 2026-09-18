fe99d8cabeaf60f371a4aad3265f17fe34476ae8
terumineko <miamonicamoura@gmail.com>
2026-09-18 21:13:19 +0100

    Add roles

--- SuperShop/Controllers/ProductsController.cs
+++ SuperShop/Controllers/ProductsController.cs
@@ -58,7 +58,7 @@ namespace SuperShop.Controllers
 
         // GET: Products/Create
 
-        [Authorize]
+        [Authorize(Roles = "Admin")]
         public IActionResult Create()
         {
             return View();
--- SuperShop/Data/SeedDb.cs
+++ SuperShop/Data/SeedDb.cs
@@ -24,6 +24,9 @@ namespace SuperShop.Data
         {
             await _context.Database.EnsureCreatedAsync();
 
+            await _userHelper.CheckRoleAsync("Admin");
+            await _userHelper.CheckRoleAsync("Customer");
+
             var user = await _userHelper.GetUserByEmailAsync("miamonicamoura@gmail.com");
             if (user == null)
             {
@@ -41,7 +44,16 @@ namespace SuperShop.Data
                 {
                     throw new InvalidOperationException("Could not create the user in seeder");
                 }
+
+                await _userHelper.AddUserToRoleAsync(user, "Admin");
             }
+
+            var isInRole = await _userHelper.IsUserInRoleAsync(user, "Admin");
+            if (!isInRole)
+            {
+                await _userHelper.AddUserToRoleAsync(user, "Admin");
+            }
+
             if (!_context.Products.Any())
             {
                 AddProduct("iPhone X", user);
